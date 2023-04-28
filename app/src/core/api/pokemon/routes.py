d89db4cef820e2c145df56d3816257a9cf3a4eec
Dimitri Topaloglou <dtopaloglou@email.com>
2023-04-28 10:10:37 -0400

    added sort

--- app/src/core/api/pokemon/routes.py
+++ app/src/core/api/pokemon/routes.py
@@ -1,7 +1,8 @@
 import logging
+import math
 from typing import List, Optional
 
-from fastapi import APIRouter, HTTPException, Body, Depends, Path
+from fastapi import APIRouter, HTTPException, Body, Depends, Path, Query
 
 from src.core.auth import get_current_user
 from src.core.db import schema, crud, models
@@ -9,6 +10,40 @@ from src.core.db.session import get_db
 
 
 pokemon_route = APIRouter(prefix="/pokemon", tags=["pokemon"])
+
+
+def camel_to_snake(s):
+    return "".join(["_" + c.lower() if c.isupper() else c for c in s]).lstrip("_")
+
+
+class Page(object):
+    def __init__(self, items, page, page_size, total):
+        self.items = items
+        self.previous_page = None
+        self.next_page = None
+        self.has_previous = page > 0
+        if self.has_previous:
+            self.previous_page = page - 1
+        previous_items = (page - 1) * page_size if page >= 1 else 0
+        self.has_next = previous_items + len(items) < total
+        if self.has_next:
+            self.next_page = page + 1
+        self.total = total
+        self.pages = int(math.ceil(total / float(page_size)))
+
+
+def paginate(query, page, page_size):
+    if page_size <= 0:
+        raise AttributeError("page_size needs to be >= 1")
+
+    items: List[models.Pokemon] = (
+        query.limit(page_size).offset((page if page > 0 else 1 - 1) * page_size).all()
+    )
+    # We remove the ordering of the query since it doesn't matter for getting a count and
+    # might have performance implications as discussed on this Flask-SqlAlchemy issue
+    # https://github.com/mitsuhiko/flask-sqlalchemy/issues/100
+    total = query.order_by(None).count()
+    return Page(items, page, page_size, total)
 
 
 @pokemon_route.get(
@@ -90,3 +125,48 @@ def update_pokemon(
         return {}
 
     raise HTTPException(status_code=404, detail=f"No pokemon found to delete")
+
+
+@pokemon_route.get(
+    "/",
+    summary="Get a list of pokemon",
+    response_model=List[schema.Pokemon],
+)
+def get_pokemon(
+    db=Depends(get_db),
+    current_page: int = Query(..., alias="page", description="Current page number."),
+    page_size: int = Query(..., alias="pageSize", description="Results per page."),
+    sort: Optional[str] = Query(
+        None,
+        description="Sort data by field and sorting direction in the form of `field:direction`",
+    ),
+):
+    """
+    Retrieve a list of all pokemon in a paginated way
+    """
+
+    query = db.query(models.Pokemon)
+
+    if sort is None:
+        query = query.order_by(models.Pokemon.name.asc())
+    else:
+        order_params = []
+
+        sort_alg = sort.split(",")
+
+        sort = [
+            {"field": s[0], "direction": s[1]} for s in [s.split(":") for s in sort_alg]
+        ]
+
+        for search_results in sort:
+            field_name = camel_to_snake(search_results.get("field"))
+            direction = search_results.get("direction")
+            r = eval(f"models.Pokemon.{field_name}.{direction}()")
+            order_params.append(r)
+
+        if len(order_params) > 0:
+            query = query.order_by(*order_params)
+
+    data = paginate(query, current_page, page_size)
+
+    return data.items
